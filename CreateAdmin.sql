670baa4fa87bd82f117870a8d25eb7c1012592f0
rqmok <rqmok@hotmail.com>
2019-10-25 17:47:39 +1100

    Add SQL script for adding an Admin.

---
+++ CreateAdmin.sql
@@ -0,0 +1,7 @@
+INSERT INTO FIT5042.USERS (email, user_type, name, password)
+    VALUES ('admin@example.com', 'A', 'Admin', 'w0BB/LTAh3ubJZ2sXORy84XpreN7fFmHMocDyFUGNX4=');
+
+INSERT INTO FIT5042.ADMINUSER VALUES ('admin@example.com');
+
+INSERT INTO FIT5042.USER_GROUPS (email, groupname)
+    VALUES ('admin@example.com', 'admin');
